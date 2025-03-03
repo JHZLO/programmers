25c873605fbe1e3858f6e1920bdc46cf2580c664
Jhzlo <010703kjh@gmail.com>
2025-03-03 20:59:51 +0900

    [level 3] Title: 단어 변환, Time: 0.19 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,49 +1,37 @@
 import java.util.*;
 
 class Solution {
+    private int minCount = Integer.MAX_VALUE;
+
     public int solution(String begin, String target, String[] words) {
-        if (!Arrays.asList(words).contains(target)) {
-            return 0;
-        }
-
-        Queue<String> que = new LinkedList<>();
         boolean[] visited = new boolean[words.length];
-        
-        que.offer(begin);
-        int cnt = 0;
+        dfs(words, visited, begin, target, 0);
+        return (minCount == Integer.MAX_VALUE) ? 0 : minCount; // 변환 불가능한 경우 0 반환
+    }
 
-        while (!que.isEmpty()) {
-            int size = que.size();
-            for (int i = 0; i < size; i++) {
-                String x = que.poll();
-                
-                if (x.equals(target)) {
-                    return cnt;
-                }
-
-                for (int j = 0; j < words.length; j++) {
-                    if (!visited[j] && canConvert(x, words[j])) {
-                        visited[j] = true;
-                        que.offer(words[j]);
-                    }
-                }
-            }
-            cnt++; 
+    private void dfs(String[] words, boolean[] visited, String begin, String target, int cnt) {
+        if (begin.equals(target)) {
+            minCount = Math.min(minCount, cnt); // 최솟값 갱신
+            return;
         }
 
-        return 0; 
+        for (int i = 0; i < words.length; i++) {
+            if (!visited[i] && canTransform(begin, words[i])) { // 한 글자만 다른 경우
+                visited[i] = true;
+                dfs(words, visited, words[i], target, cnt + 1);
+                visited[i] = false; // 백트래킹
+            }
+        }
     }
-    
-    public boolean canConvert(String x, String word) {
+
+    private boolean canTransform(String s1, String s2) {
         int diff = 0;
-        for (int i = 0; i < x.length(); i++) {
-            if (x.charAt(i) != word.charAt(i)) {
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
                 diff++;
             }
-            if (diff > 1) {
-                return false;
-            }
+            if (diff > 1) return false; // 2개 이상 다르면 변환 불가
         }
-        return diff == 1;
+        return diff == 1; // 정확히 1개의 문자만 다를 때 변환 가능
     }
 }
