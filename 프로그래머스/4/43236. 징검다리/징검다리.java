0208b85f11f52432161855a0dcb99af7ee864be5
Jhzlo <010703kjh@gmail.com>
2025-03-22 16:54:55 +0900

    [level 4] Title: 징검다리, Time: 36.23 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -17,14 +17,14 @@ class Solution {
     public int solution(int distance, int[] rocks, int n) {
         int answer = 0;
         
-        Arrays.sort(rocks);
+        Arrays.sort(rocks); // 이분탐색 -> 정렬 필수
         
         int left = 0;
         int right = distance;
         
         while (left <= right){
             int mid = (left + right) / 2;
-            if (getCnt(rocks, distance, mid) <= n){
+            if (getCnt(rocks, distance, mid) <= n){ // 만족하는 경우 answer = mid
                 answer = mid;
                 left = mid + 1;
             }else{
@@ -39,7 +39,7 @@ class Solution {
         int remove = 0;
         int end = distance;
         
-        for (int i = 0; i < rocks.length; i++){
+        for (int i = 0; i < rocks.length; i++){ // mid보다 작은 경우 돌맹이 제거
             if (rocks[i] - before < mid){
                 remove ++;
                 continue;
@@ -47,7 +47,7 @@ class Solution {
             before = rocks[i];
             
         }
-        if (end - before < mid){
+        if (end - before < mid){ // 마지막 돌맹이는 끝 지점과의 간격이므로 따로 카운트
             remove ++;
         }
         
