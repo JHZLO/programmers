b8843b6b7527b91103c83c3e23de37987a932f18
Jhzlo <010703kjh@gmail.com>
2025-04-10 01:47:49 +0900

    Update 가장 긴 증가하는 부분 수열.java

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -42,3 +42,43 @@ public class Main {
 	}
  
 }
+
+
+----
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		int[] dp = new int[n];
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			dp[i] = 1; // 초기값 1로 설정
+		}
+
+		for (int i = 1; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				if (arr[i] > arr[j]) {
+					dp[i] = Math.max(dp[i], dp[j] + 1);
+				}
+			}
+		}
+
+		int max = 0;
+		for (int i = 0; i < n; i++) {
+			max = Math.max(max, dp[i]);
+		}
+
+		System.out.println(max);
+	}
+}
