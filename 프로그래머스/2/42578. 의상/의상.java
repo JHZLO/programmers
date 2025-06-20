0a938aba5cc8c3b74a6fc254d0ebebe81b2b4986
Jhzlo <010703kjh@gmail.com>
2025-06-20 19:00:25 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,19 +3,15 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        
         Map<String, Integer> clothesMap = new HashMap<>();
         
-        for (String[] cloth : clothes) {
+        for (String[] cloth: clothes){
             clothesMap.put(cloth[1], clothesMap.getOrDefault(cloth[1], 0) + 1);
         }
         
-        for (int count : clothesMap.values()) {
-            // 각 카테고리의 아이템 개수 + 선택하지 않는 경우(1)
-            answer *= (count + 1);
+        for (int value: clothesMap.values()){
+            answer *= value + 1;
         }
-        
-        // 모든 카테고리에서 아무것도 선택하지 않는 경우 제외
         answer -= 1;
         
         return answer;
