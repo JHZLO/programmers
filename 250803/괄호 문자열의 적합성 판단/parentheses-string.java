c63774ee3e33fd0cb56e5d5578e937ecb8c6b13e
Jhzlo <010703kjh@gmail.com>
2025-08-03 19:32:34 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 116ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250803/괄호 문자열의 적합성 판단/parentheses-string.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < str.length(); i++){
+            char x = str.charAt(i);
+            if (x == '('){
+                stack.push(x);
+            }else{
+                stack.pop();
+            }
+        }
+
+        if (stack.isEmpty()){
+            System.out.print("Yes");
+        }else{
+            System.out.print("No");
+        }
+    }
+}
